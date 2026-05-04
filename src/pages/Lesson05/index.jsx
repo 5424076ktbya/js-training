2ebef60c3020e7b0c8086901ae1a11193113a8e9
5424076ktbya <chya24076@g.nihon-u.ac.jp>
2026-05-04 16:37:20 +0900

    Lesson5完了

--- src/pages/Lesson05/index.jsx
+++ src/pages/Lesson05/index.jsx
@@ -4,9 +4,24 @@ import Chart from "../../components/Chart05";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const genders = Array.from(new Set(input.map(({ gender }) => gender))); //性別一覧を作る
+  const min = Math.round(Math.min(...input.map(({ y }) => y))); //最小値
+  const max = Math.round(Math.max(...input.map(({ y }) => y))); //最大値
+  const bins = Array.from({ length: max - min + 1 }).map((_, i) => { //空のビン（箱）を作る
+    const obj = { //ビン1個の中身
+      bin: (min + i).toString(),
+    };
+    for (const gender of genders) { //男女のカウントを0で初期化
+      obj[gender] = 0;
+    }
+    return obj;
+  });
+  for (const { y, gender } of input) { //データを1人ずつ入れる
+    const i = Math.round(y) - min; //どの箱に入れるか計算
+    bins[i][gender] += 1; //カウントを増やす
+  }
+  return bins;
 };
-
 const Lesson = () => {
   return (
     <LessonPage
