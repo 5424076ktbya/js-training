8cf02d3c4f0b691e1f16fe5c82785dda25b57764
5424076ktbya <chya24076@g.nihon-u.ac.jp>
2026-05-07 01:25:57 +0900

    Lesson4修正

--- src/pages/Lesson04/index.jsx
+++ src/pages/Lesson04/index.jsx
@@ -4,13 +4,13 @@ import Chart from "../../components/Chart04";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  const species = Array.from(new Set(input.map(({species}) => species)));
-  return species.map((species) => {
+  const species = Array.from(new Set(input.map(({species}) => species)));  //input.map(({ species }) => species)は各データから 品種名だけ取り出す、new Set(...)は重複を消す、Array.from(...)はSetは配列じゃないので、配列に戻す
+  return species.map((species) => {  //品種ごとに処理する
     return {
-      id: species,
+      id: species,  //出力形式の id に品種名を入れる
       data: input
-        .filter((item) => item.species === species)
-        .map(({ sepalLength: x, petalWidth: y }) => ({ x, y })),
+        .filter((item) => item.species === species)  //今の品種だけ抽出する
+        .map(({ sepalLength: x, petalWidth: y }) => ({ x, y })),  //データの形を変換する
     };
   });
 };
